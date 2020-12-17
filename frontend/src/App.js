2616f198a480a21280d5d49354d31e50fed9725a
David Mulholland <davidmulholland@Davids-iMac.local>
2020-12-17 14:22:01 +0000

    front end connected to a backend server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './components/ProductScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route exact path='/product/:id' component={ProductScreen} />
+          <Route exact path='/' component={HomeScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
